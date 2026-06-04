7321a21f9e53a217bc69af31e3f82ef4ed0149a5
prachi23gupta <prachi2022gupta@gmail.com>
2026-06-04 23:43:08 +0530

    Add matrix zeroes setting algorithm in better.cpp
    Implement a solution to set matrix zeroes based on zero elements in the input matrix.

---
+++ Day-04/SetMatrixZeroes/better.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<vector<int>> matrix = {
+        {1,1,1},
+        {1,0,1},
+        {1,1,1}
+    };
+
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+
+            if(matrix[i][j] == 0){
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+
+            if(row[i] == 1 || col[j] == 1){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    for(auto r : matrix){
+        for(auto ele : r){
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// Time Complexity = O(m*n)
+// Space Complexity = O(m+n)
