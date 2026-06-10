e3360c9701ec13aa6cf05b34bbe74430371e81aa
prachi23gupta <prachi2022gupta@gmail.com>
2026-06-10 22:42:04 +0530

    Add brute force method to count subarrays with XOR K
    Implement brute force solution to count subarrays where the XOR equals K.

---
+++ Day-10/Count-Subarrays-with-Sum-XOR-as-K/brute.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Count Subarrays with Sum XOR as K
+
+int main(){
+
+    vector<int> arr = {4, 2, 2, 6, 4};
+    int n = arr.size();
+
+    int k;
+    cout << "Enter the value of K: ";
+    cin >> k;
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        
+        for(int j = i; j < n; j++){
+
+            int xr = 0;
+
+            for(int l = i; l <= j; l++){
+
+                xr ^= arr[l];
+            }
+
+            if(xr == k){
+                count++;
+            }
+            
+        }
+    }
+    cout << count << endl;
+
+    return 0;
+}
+
+// Time Complexity: O(n^3)
+// Space Complexity: O(1)
