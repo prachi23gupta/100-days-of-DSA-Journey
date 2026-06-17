b09567d6133c82634a465d8f2aaba15d79a4c1b9
prachi23gupta <prachi2022gupta@gmail.com>
2026-06-17 18:01:03 +0530

    Implement count occurrence in sorted array
    This program counts the occurrences of a given number in a sorted array using binary search. It finds both the first and last occurrence of the number to calculate the total count.

---
+++ Day-17/Count-Occurence-in-an-array/code.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Count occurence in an array
+*/
+
+int main(){
+
+    vector<int> arr = {2, 4, 6, 8, 8, 8, 11, 13};
+    int n = arr.size();
+
+    int start = 0;
+    int end = n - 1;
+
+    int x;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    int first = -1;
+    int last = -1;
+
+    while (start <= end) {
+
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == x){
+            first = mid;
+            end = mid - 1;
+        }
+        else if(arr[mid] > x){
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+
+    // 🔥 if first occurence ni mila to mtlb x h hi ni so no need to go for last occurence
+    if(first == -1){
+        return 0;
+    }
+
+    start = 0;
+    end = n - 1;
+
+     while (start <= end) {
+
+        int mid = start + (end - start) / 2;
+
+        if(arr[mid] == x){
+            last = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] < x){
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+
+    cout << last - first + 1; // because eg: x = 8 to 8 lgatar 3 honge scatered ni honge
+    // isliye last - first and + 1 because index ek count peeche chlta h
+
+    return 0;
+}
+
+// Time Complexity: O(log n) // base 2
+// Space Complexity: O(1)
