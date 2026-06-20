2bb382f1e87a59671ec19a559b863cd1a40c00d7
prachi23gupta <prachi2022gupta@gmail.com>
2026-06-20 23:16:24 +0530

    Add peak element finder using binary search
    Implement binary search to find a peak element in an array.

---
+++ Day-20/Peak-Element/optimal.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+
+    if (n == 1) return 0;
+
+    if (nums[0] > nums[1]) return 0;
+    if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+    int low = 1, high = n - 2;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] > nums[mid - 1] &&
+            nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+
+        if (nums[mid] > nums[mid - 1]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+
+    cout << findPeakElement(nums);
+
+    return 0;
+}
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
