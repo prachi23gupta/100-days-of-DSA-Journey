3c173ce7fd75abe209573812da7a8c0c20790e2a
prachi23gupta <prachi2022gupta@gmail.com>
2026-09-08 20:56:15 +0530

    Add header comment for Integer to Roman function
    Added header comment for clarity.

---
+++ Day-98/Integer to Roman/optimal.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Integer to Roman
+
+string intToRoman(int num) {
+    int values[] = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4, 1
+    };
+
+    string symbols[] = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    };
+
+    string ans;
+
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            ans += symbols[i];
+            num -= values[i];
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int num;
+    cin >> num;
+
+    cout << intToRoman(num);
+
+    return 0;
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1) extra space
